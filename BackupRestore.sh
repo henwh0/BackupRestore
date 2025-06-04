619c32e3b1dea23c04afaff74b69723c14471b8b
henwh0 <hmcginnis96@gmail.com>
2025-06-04 16:22:48 -0400

    Update BackupRestore.sh

--- BackupRestore.sh
+++ BackupRestore.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-BACKUP_FILE="*.tar.gz"
+BACKUP_LOCATION="/backup"
 RESTORE_DIR="/"
-# Check that backup file exists
-if [ ! -f "$BACKUP_FILE" ]; then
-    echo "ERROR: Backup file not found."
+# Check that backup directory exists
+if [ ! -d "$BACKUP_LOCATION" ]; then
+    echo "ERROR: Backup directory not found."
     exit 1
 fi
-# Extracting backup
-echo "Extracting backup.."
-tar -xvf "$BACKUP_FILE" -C "$RESTORE_DIR"
-# Restore system files
-echo "Restoring system files.."
+# Loop through each backup file
+for FILE in "$BACKUP_LOCATION"/*.tar.gz; do
+# Get directory name 
+    DIRECTORY_NAME=$(basename "$FILE" | cut -d '-' -f 1)
+    # Check that file is a valid backup
+    if [ -f "$FILE" ]; then
+        # Restore
+        echo "Extracting $DIRECTORY_NAME backup.."
+        tar -xvf "$FILE" -C "$RESTORE_DIR"
+        # Check status
+        if [ $? -eq 0 ]; then
+            echo "Restoration of $DIRECTORY_NAME successful."
+        else
+            echo "ERROR: Restoration of $DIRECTORY_NAME failed."
+        fi
+    else
+        echo "WARNING: $FILE does not exist or is not a file."
+    fi
+done
+echo "Restoration script complete."
