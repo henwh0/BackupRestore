38145058fc9bdf1b5035957d73a5e6dc1b149608
henwh0 <hmcginnis96@gmail.com>
2025-06-12 22:54:09 -0400

    Update BackupRestore.sh

--- BackupRestore.sh
+++ BackupRestore.sh
@@ -2,26 +2,37 @@
 
 BACKUP_LOCATION="/backup"
 RESTORE_DIR="/"
+
+# Please run as root!
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run script as root!"
+    exit 1
+fi
 # Check that backup directory exists
 if [ ! -d "$BACKUP_LOCATION" ]; then
-    echo "ERROR: Backup directory not found."
-    exit 1
+    echo "Backup location not found.."
+    echo "Creating backup location: $BACKUP_LOCATION"
+    if ! mkdir -p "$BACKUP_LOCATION"; then
+        echo "ERROR: Failed to create backup location at $BACKUP_LOCATION"
+        exit 1
+    fi
+    echo "Backup location created successfully!"
 fi
 # Confirmation
 read -p "WARNING: This script will restore files directly to /. Continue? (y/n): " CONFIRM
-if [[ "$CONFIRM" != [yY] ]]; then
+if [[ ! "$CONFIRM" =~ ^[yY](es)?$ ]]; then
     echo "Script aborted by user."
     exit 1
 fi
 # Loop through each backup file
 for FILE in "$BACKUP_LOCATION"/*.tar.gz; do
 # Get directory name 
-    DIRECTORY_NAME=$(basename "$FILE" | cut -d '-' -f 1)
+    DIRECTORY_NAME=$(basename "$FILE" .tar.gz)
     # Check that file is a valid backup
     if [ -f "$FILE" ]; then
         # Restore
         echo "Extracting $DIRECTORY_NAME backup.."
-        pv "$FILE" | gunzip | tar -xf - -C "$RESTORE_DIR"
+        pv "$FILE" | tar -xz -C "$RESTORE_DIR"
         # Check status
         if [ $? -eq 0 ]; then
             echo "Restoration of $DIRECTORY_NAME successful."
